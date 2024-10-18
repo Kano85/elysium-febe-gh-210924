d5a4f2c2b55a5c54a91c3278c14bcec884518206
harold.canocardenas <Harold.CanoCardenas@wbstraining.de>
2024-10-18 08:29:39 +0200

    Fix Fix import paths in sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,11 +2,7 @@
 import { visionTool } from '@sanity/vision';
 import { defineConfig } from 'sanity';
 import { structureTool } from 'sanity/structure';
-import {
-  apiVersion,
-  dataset,
-  projectId,
-} from '../elysium-febe-210924/src/sanity/env';
+import { apiVersion, dataset, projectId } from './src/sanity/env';
 import { schema } from './src/sanity/schemaTypes';
 import { structure } from './src/sanity/structure';
 
