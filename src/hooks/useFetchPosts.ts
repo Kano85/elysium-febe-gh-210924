3f64f0c0dea92c49fd076604f13fd598350d312f
Kano85 <afte_85@hotmail.com>
2025-04-26 09:49:52 +0200

    refactor: remove unused useTranslation import from useFetchPosts hook

--- src/hooks/useFetchPosts.ts
+++ src/hooks/useFetchPosts.ts
@@ -3,11 +3,9 @@ import { useState, useEffect } from 'react';
 import { client } from '@/sanity/lib/client';
 import { ALL_POSTS_QUERY } from '@/sanity/lib/queries';
 import { ALL_POSTS_QUERYResult } from '@/sanity/types';
-import { useTranslation } from 'react-i18next';
 
 const useFetchPosts = (selectedLanguageFullForm: string) => {
   // Use ALL_POSTS_QUERYResult (which is already an array)
-  const { i18n } = useTranslation();
   const [posts, setPosts] = useState<ALL_POSTS_QUERYResult>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
